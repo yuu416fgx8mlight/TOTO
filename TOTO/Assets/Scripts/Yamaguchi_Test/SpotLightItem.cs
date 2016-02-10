23f69f5107d04268669ccd3c6f4e6d2d78d3ecb7
yuta <yamaguchiyuuta@yamaguchiyuuta.local>
2016-02-10 14:27:29 +0900

    アイテム「スポットライト」の実装

---
+++ TOTO/Assets/Scripts/Yamaguchi_Test/SpotLightItem.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class SpotLightItem : MonoBehaviour {
+
+	public Image RightSpot;
+	public Image LeftSpot;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void OnTriggerEnter(Collider SpotItemGet){
+		if (gameObject.tag == "LeftPlayer") {
+			StartCoroutine ("LeftSpotItem");
+		}
+		if (gameObject.tag == "RightPlayer") {
+			StartCoroutine ("RightSpotItam");
+		}
+	}
+
+	//左側の塔でアイテムを取得した場合の処理
+	public IEnumerator LeftSpotItem(){
+		RightSpot.enabled = true;
+		yield return new WaitForSeconds(10.0f);
+		RightSpot.enabled = false;
+	}
+
+	//右側の塔でアイテムを取得した場合の処理
+	public IEnumerator RightSpotItem(){
+		LeftSpot.enabled = true;
+		yield return new WaitForSeconds(10.0f);
+		LeftSpot.enabled = false;
+	}
+}
