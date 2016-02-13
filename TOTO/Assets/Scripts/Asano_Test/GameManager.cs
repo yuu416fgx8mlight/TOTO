eee1b07d9e454e639e43aa37ed5031a313bc0bce
yuta <yamaguchiyuuta@yamaguchiyuuta.local>
2016-02-13 16:54:54 +0900

    オブジェクトの仮配置 , 一応遊べるように（ただし2P , テメーはダメだ）

--- TOTO/Assets/Scripts/Asano_Test/GameManager.cs
+++ TOTO/Assets/Scripts/Asano_Test/GameManager.cs
@@ -1,10 +1,14 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour {
 
 	[Header("制限時間")]
 	[SerializeField]public float Timer = 180;
+
+	[Header("タイマー")]
+	[SerializeField]public Text TimerText;
 
 	//ゲーム中かどうか
 	public static bool IsGame;
@@ -16,6 +20,9 @@ public class GameManager : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		//シーン遷移用ATフィールド
 		DontDestroyOnLoad (this.gameObject);
+		Timer -= Time.deltaTime * 1.0f;
+		TimerText.GetComponent<Text>().text = ((int)Timer).ToString();
 	}
 }
